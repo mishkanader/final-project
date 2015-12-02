96f2c72b1d8e3823705c03ed9603d18d908cfec9
Lauren Ogawa <logawa12@yahoo.com>
2015-12-01 23:44:38 -1000

    Update register.php
    Final

--- register.php
+++ register.php
@@ -1,52 +1,30 @@
 <?php
-//session_start(); // Starting Session
-//$error=''; // Variable To Store Error Message
-//if (isset($_POST['submit'])) {
-//    if (empty($_POST['username']) || empty($_POST['password'])) {
-//        $error = "Username or Password is invalid";
-//   }
-//   else
-//  {
 
-//Everything connects, but i cant get the sql to check if the user is valid
-//correctly.  it goes strait to the page without error checking.
+        //i commented these out because i couldn't differentiate between
+//MySQL password and password the user is inputting.
 
-// Define $username and $password
+// Define username, password, and email from from fields
 $username=$_POST['username'];
 $password=$_POST['password'];
 $email=$_POST['email'];
-// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-//
-//        $connection = mysql_connect("localhost", "root", "cruelangel");
-$connection = mysql_connect("localhost", "root", "");
-// To protect MySQL injection for Security purpose
-$username = stripslashes($username);
-$password = stripslashes($password);
-$username = mysql_real_escape_string($username);
-$password = mysql_real_escape_string($password);
-// Selecting Database
-$db = mysql_select_db("derpderp", $connection);
 
-// SQL query to fetch information of registerd users and finds user match.
-$query = "INSERT INTO $db (username, password, email) VALUES ($username, $password, $email)";
-
-if ($query == TRUE && $username != "") {
-    $_SESSION['login_user']=$username; // Initializing Session
-    $_SESSION['logged_in']=TRUE;
-    echo 'Registered successfully.';
-    print "<h1>" . "Registered Successfully!!" . "</h1>";
-    header("Refresh: 3; url=login.html");// Redirecting To Other Page
-} else {
-    print "<h1>" . "Error: redirecting to login page..." . "</h1>";
-    header("Refresh: 3; url=login.html");
+// Create connection
+//$conn = new mysqli("localhost", "root", "cruelangel", "derpderp");
+$conn = new mysqli("localhost", "root", "", "derpderp");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-mysql_close($connection); // Closing Connection
 
+$sql = "INSERT INTO login (username, password, email)
+VALUES ('$username', '$password', '$email')";
 
+if ($conn->query($sql) == TRUE) {
+	header("Refresh: 5; url=login.html");
+	echo 'Registered successfully.';
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
-//    }
-//}
+$conn->close();
 ?>
-
-
-
